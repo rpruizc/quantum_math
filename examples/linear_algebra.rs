b2bf4bfab5e2636daaee9c28772364959c8af493
Rodolfo Ruiz <rpruiz@me.com>
2021-07-06 17:58:51 -0500

    Multiplication with scalars, vectors and matrices

--- examples/linear_algebra.rs
+++ examples/linear_algebra.rs
@@ -1,12 +1,13 @@
 // Uses the ndarray crate
-use ndarray::arr2;
+use ndarray::{arr1, arr2, Array1};
 
 fn main() {
+    let scalar = 8;
+    let vector = arr1(&[0, 1, 2]);
     let array1 = arr2(&[[1, 2, 3],
                                        [4, 5, 6]]);
     let array2 = arr2(&[[4, 3, 1,],
                                        [7, 8, 6]]);
-
     let array3 = arr2(&[[2, 3],
                                        [6, 9],
                                        [1, 5]]);
@@ -15,6 +16,10 @@ fn main() {
 
     // Matrix multiplication is performed with ndarray::ArrayBase::dot
     let multiplication = array1.dot(&array3);
+
+    // Multiply a scalar with a vector with a matrix
+    let new_vector: Array1<_> = scalar * vector;
+    let new_matrix = array1.dot(&new_vector);
 
     println!("===== Sum =====");
     println!("{}", array1);
@@ -29,5 +34,9 @@ fn main() {
     println!("{}", array3);
     println!("=");
     println!("{}", multiplication);
+
+    println!("New Vector: {}", new_vector);
+    println!("New Matrix: {}", new_matrix);
+
 
 }
